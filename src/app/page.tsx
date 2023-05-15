a52a1ff1267767fa52ef5d20228c09ba824e4fde
oleksandrdashevskyi <oleksandr.dashevskyi@andorhealth.com>
2023-05-15 12:57:45 -0400

    set prettier rules.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 import Paragraph from "@/app/components/ui/Paragraph";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
-  return (
-    <main className="bg-red-500">
-      <Paragraph size="sm" />
-    </main>
-  )
+    return (
+        <main className="bg-red-500">
+            <Paragraph size="sm"/>
+        </main>
+    )
 }
