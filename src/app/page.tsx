375619dbecfa8911565d88310cea18ea30274f39
oleksandrdashevskyi <oleksandr.dashevskyi@andorhealth.com>
2023-05-15 23:29:41 -0400

    configured prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Paragraph from "@/app/components/ui/Paragraph";
 export default function Home() {
     return (
         <main className="bg-red-500">
-            <Paragraph size="sm">Some Text here</Paragraph>
+
         </main>
     )
 }
