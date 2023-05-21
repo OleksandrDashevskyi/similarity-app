e2b0c4517738c73c757336636fb3b58879e9127d
oleksandrdashevskyi <oleksandr.dashevskyi@andorhealth.com>
2023-05-21 15:47:34 -0400

    started creating NavBar components with reusable buttons moved utils to src main folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
-import { cn } from "@/app/lib/utils";
+import { cn } from "@/lib/utils";
+import Providers from "@/components/ui/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn("bg-white text-slate-900 antialiased")}>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn("bg-white text-slate-900 antialiased", inter.className)}
+    >
+      <body className="min-h-screen bg-slate-50 dark:bg-slate-100 antialiased">
+        <Providers>
+          {children}
+          {/*<Navbar />*/}
+        </Providers>
+        {/* Allow for more height on mobile devices */}
+        <div className="h-40 md:hidden" />
+      </body>
     </html>
   );
 }
